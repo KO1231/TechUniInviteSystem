964bb585032ebd69e82fa20619730bc734b50b18
KO1231 <k.oka@kazutech.jp>
2025-03-02 23:23:30 +0900

    add: state table

--- src/main/resources/db/migration/V0.1.0__create_invite_discord_table.sql
+++ src/main/resources/db/migration/V0.1.0__create_invite_discord_table.sql
@@ -1,13 +1,25 @@
 /*
  GRANT SELECT, INSERT, CREATE, REFERENCES, ALTER ON DB.invite_discord TO DBUSER;
+ GRANT SELECT, INSERT, CREATE, REFERENCES, ALTER ON DB.invite_discord TO DBUSER;
+
  GRANT SELECT, INSERT, CREATE, REFERENCES, ALTER ON DB_dev.invite_discord TO DBUSER_dev;
+ GRANT SELECT, INSERT, CREATE, REFERENCES, ALTER ON DB_dev.invite_discord_state TO DBUSER_dev;
  */
 
 INSERT INTO target_app (name) VALUES ('discord');
 
 CREATE TABLE invite_discord (
-    id INT PRIMARY KEY,
+    invite_id INT PRIMARY KEY,
     guild_id BIGINT UNSIGNED NOT NULL,
 
-    FOREIGN KEY (id) REFERENCES invite(id)
+    FOREIGN KEY (invite_id) REFERENCES invite(id)
+);
+
+CREATE TABLE invite_discord_state (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    invite_id INT NOT NULL,
+    state VARCHAR(36) NOT NULL UNIQUE,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (invite_id) REFERENCES invite_discord(invite_id)
 );
--- src/main/resources/db/migration_dev/V0.1.1__add_sample_discord_invite.sql
+++ src/main/resources/db/migration_dev/V0.1.1__add_sample_discord_invite.sql
@@ -2,6 +2,6 @@ INSERT INTO invite(id, code, search_id, is_disabled, target_app_id, expires_at) 
 (1, 'b713ea7a-c273-4112-9183-d9f0de3cecf8', 'sample_search_id1', FALSE, 1, NULL),
 (2, '7efd92ff-914e-40e2-8855-748126dadc62', 'sample_search_id2', TRUE, 1, NULL);
 
-INSERT INTO invite_discord(id, guild_id) VALUES
+INSERT INTO invite_discord(invite_id, guild_id) VALUES
 (1, 1236697356783587328),
 (2, 1236697356783587328);
