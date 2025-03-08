aac69fa179895b988844ba88df1857596d0aecd4
KO1231 <k.oka@kazutech.jp>
2025-03-08 22:25:32 +0900

    add: checkBotHasPermissionのearly check

--- src/main/java/org/techuni/TechUniInviteSystem/service/DiscordAPIService.java
+++ src/main/java/org/techuni/TechUniInviteSystem/service/DiscordAPIService.java
@@ -39,6 +39,11 @@ public class DiscordAPIService {
                 .map(MemberData::roles) //
                 .orElseThrow(() -> ErrorCode.DISCORD_GUILD_ACCESS_ERROR.exception(String.valueOf(guildId)));
 
+        // サーバーに入っているならば、expected none -> always true
+        if (expected.isEmpty()) {
+            return true;
+        }
+
         // hasRolesがemptyのとき、必ずあるはずのBOT権限も取得できていない -> ロール管理権限がない。
         // ロール管理権限がないときは、ロールによるエラーハンドリングを諦めて招待実行時エラーによるハンドリングで運用する。(最小権限のみを必要とするという非機能要件による)
         if (hasRoles.isEmpty()) {
